92051205d67aaf452fe42095f8c056496f4b542c
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2017-02-22 21:54:13 +0100

    add vectorized regularized logistic regression

--- week4/ex3/lrCostFunction.m
+++ week4/ex3/lrCostFunction.m
@@ -1,14 +1,14 @@
 function [J, grad] = lrCostFunction(theta, X, y, lambda)
-%LRCOSTFUNCTION Compute cost and gradient for logistic regression with 
+%LRCOSTFUNCTION Compute cost and gradient for logistic regression with
 %regularization
 %   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -25,21 +25,38 @@ grad = zeros(size(theta));
 %
 %       Each row of the resulting matrix will contain the value of the
 %       prediction for that example. You can make use of this to vectorize
-%       the cost function and gradient computations. 
+%       the cost function and gradient computations.
 %
-% Hint: When computing the gradient of the regularized cost function, 
+% Hint: When computing the gradient of the regularized cost function,
 %       there're many possible vectorized solutions, but one solution
 %       looks like:
 %           grad = (unregularized gradient for logistic regression)
-%           temp = theta; 
-%           temp(1) = 0;   % because we don't add anything for j = 0  
+%           temp = theta;
+%           temp(1) = 0;   % because we don't add anything for j = 0
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
 
+h = sigmoid(X * theta);
 
+a = -y' * log(h);
+b = (1-y') * log((1-h))
+error = a - b;
 
+% We want the regularization to exclude the bias feature, so we can set theta(1) to zero.
+theta(1) = 0;
 
+% Now we need to calculate the sum of the squares of theta.
+% Since we've set theta(1) to zero, we can square the entire theta vector.
+% If we vector-multiply theta by itself, we will calculate the sum automatically.
+sum_theta_squared = theta' * theta;
+reg_term = sum_theta_squared * (lambda / (2 * m));
+
+J = sum(error) / m + reg_term;
+
+another_error = h - y;
+tmp = (sum(another_error .* X) / m) + theta' * (lambda / m);
+grad = tmp'
 
 
 
