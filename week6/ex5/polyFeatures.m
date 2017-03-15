1b66bc40feb6e99266f1a2d031f9578837934c2d
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2017-03-15 17:15:55 +0100

    polyFeatures working

--- week6/ex5/polyFeatures.m
+++ week6/ex5/polyFeatures.m
@@ -7,18 +7,22 @@ function [X_poly] = polyFeatures(X, p)
 
 
 % You need to return the following variables correctly.
-X_poly = zeros(numel(X), p);
+X_poly = zeros(numel(X), p)
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Given a vector X, return a matrix X_poly where the p-th 
+% Instructions: Given a vector X, return a matrix X_poly where the p-th
 %               column of X contains the values of X to the p-th power.
 %
-% 
+% %
 
-
-
-
-
+m = size(X, 1);
+for v = 1:m
+  tmp = zeros(1, p);
+  for i = 1:p
+    tmp(i) = X(v) ^ i;
+  end
+  X_poly(v, :) = tmp;
+end
 
 % =========================================================================
 
