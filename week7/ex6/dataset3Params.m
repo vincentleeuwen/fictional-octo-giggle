91f58d7e8af939a95a26afb1ab9ef77c3cc2d79a
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2017-04-03 19:34:07 +0200

    1.2.3 succesfully submitted

--- week7/ex6/dataset3Params.m
+++ week7/ex6/dataset3Params.m
@@ -2,8 +2,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
@@ -11,15 +11,38 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 C = 1;
 sigma = 0.3;
 
+Cvals = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigmavals = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+
+Clength = size(Cvals, 2);
+sigmalength = size(sigmavals, 2);
+
+prediction_error = 1;
+for Cindex = 1:Clength
+  for sigmaindex = 1:sigmalength
+    C_tmp = Cvals(Cindex);
+    sigma_tmp = sigmavals(sigmaindex);
+    model = svmTrain(X, y, C_tmp, @(x1, x2) gaussianKernel(x1, x2, sigma_tmp));
+    predictions = svmPredict(model, Xval);
+    new_prediction_error = mean(double(predictions ~= yval));
+    if (new_prediction_error < prediction_error)
+      prediction_error = new_prediction_error;
+      C = C_tmp;
+      sigma = sigma_tmp;
+    end
+  end
+end
+
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
 
